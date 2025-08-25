fff1d7d2c1e8dc8b5b13c888b0a76d6ca9298af3
Iris Delorme <2436300@cegeplimoilou.ca>
2025-08-25 13:15:50 -0400

    playing around + create 'formatif_1'

--- main.py
+++ main.py
@@ -9,8 +9,17 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+input("message?")
+print("yes?")
+import formatif_1
+
+
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+
+
+#can only call things higher??
+
+#wtf is this "main" interface??-> it's not even needed??????????
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
